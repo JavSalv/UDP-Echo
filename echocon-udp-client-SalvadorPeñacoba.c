16b268db68bb24bf6b9b5cc47f1e6a1c85c13168
JavSalv <javiersalvadorpenacoba@gmail.com>
2023-10-16 13:12:46 +0200

    Paso a memoria dinámica

--- echocon-udp-client-SalvadorPeñacoba.c
+++ echocon-udp-client-SalvadorPeñacoba.c
@@ -10,12 +10,16 @@
 //Macro assert para comprobar + mensaje de error.
 #define ASSERT(_bool, ...) if (!(_bool)) { fprintf(stderr, __VA_ARGS__); exit(EXIT_FAILURE); }
 
+#define MAX_LENGHT 80
+
 int main(int argc, char** argv){
 
     uint16_t puerto = htons(5);
     struct in_addr addr;
-    char msg_in[80];
-    char msg_out[80];
+
+    char* msg_in2;
+    char* msg_out2;
+
     int aux;
     int sockfd;
     struct sockaddr_in server_addr;
@@ -25,13 +29,16 @@ int main(int argc, char** argv){
     memset(&server_addr,0,sizeof(server_addr));
     memset(&my_addr,0,sizeof(my_addr));
 
+
     ASSERT((argc== 3 || argc== 5),"Uso: %s ip_servidor [-p puerto_servidor] cadena\n", argv[0]);
 
     ASSERT((inet_aton(argv[1],&addr) == 1),"Uso: %s ip_servidor [-p puerto_servidor] cadena\n", argv[0]);
     
     aux = (argc == 3) ? 2 : 4;
-    ASSERT((strlen(argv[aux]) < 80),"Error: La longitud de cadena debe ser < 80 char\n")
-    strncpy(msg_out,argv[aux],80);
+    if(strlen(argv[aux]) > MAX_LENGHT)
+        printf("La cadena será truncada a %i caracteres.\n",MAX_LENGHT);
+    msg_out2 = strndup(argv[aux],MAX_LENGHT);
+    msg_in2= calloc(MAX_LENGHT+1,sizeof(char));
 
     if(argc == 5){
         ASSERT((strcmp(argv[2],"-p")== 0), "Uso: %s ip_servidor [-p puerto_servidor] cadena\n", argv[0]);
@@ -39,7 +46,7 @@ int main(int argc, char** argv){
         ASSERT((aux > 0 && aux <= 65535),"Error: puerto_servidor no es un puerto válido\n");
         puerto = htons(aux);
     }
-    printf("Puerto: %i, Cadena: %s\n",ntohs(puerto),msg_out);
+    printf("Puerto: %i, Cadena: %s\n",ntohs(puerto),msg_out2);
 
     //Poblamos el struct de la direccion del socket servidor.
     server_addr.sin_port = puerto;
@@ -52,14 +59,14 @@ int main(int argc, char** argv){
     aux = bind(sockfd, (struct sockaddr*) &my_addr, sizeof(my_addr));
     ASSERT(aux == 0,"Error vinculando socket: %s\n", strerror(errno));
 
-    aux = sendto(sockfd, msg_out, 80,0, (struct sockaddr*)&server_addr,sizeof(server_addr));
+    aux = sendto(sockfd, msg_out2, MAX_LENGHT,0, (struct sockaddr*)&server_addr,sizeof(server_addr));
     ASSERT(aux != -1,"Error enviando datagrama: %s\n",strerror(errno));
     printf("Cadena enviada, esperando respuesta...\n");
 
-    aux = recvfrom(sockfd,msg_in,80,0,NULL,NULL);
+    aux = recvfrom(sockfd,msg_in2,MAX_LENGHT,0,NULL,NULL);
     ASSERT(aux != -1,"Error recibiendo datagrama: %s\n",strerror(errno));
 
-    printf("Cadena recibida: %s\n",msg_in);
+    printf("Cadena recibida: %s\n",msg_in2);
 
     close(sockfd);
 
--- udp-server.c
+++ udp-server.c
@@ -15,14 +15,19 @@
         exit(EXIT_FAILURE);           \
     }
 
+#define MAX_LENGHT 80
+
 void process_string();
+
 
 int main(int argc, char **argv)
 {
 
+    char* msg_in2;
+    char* msg_out2;
+
     uint16_t puerto = htons(2000);
-    char msg_in[80];
-    char msg_out[80];
+
     int aux;
     int sockfd;
     unsigned int client_legth = 0;
@@ -31,6 +36,9 @@ int main(int argc, char **argv)
 
     memset(&client_addr,0,sizeof(client_addr));
     memset(&my_addr,0,sizeof(my_addr));
+
+    msg_in2 = (char*)calloc(MAX_LENGHT+1,sizeof(char));
+    msg_out2 = (char*)calloc(MAX_LENGHT+1,sizeof(char));
 
     ASSERT((argc == 1 || argc == 3), "Uso: %s [-p puerto_servidor]\n", argv[0]);
 
@@ -59,22 +67,24 @@ int main(int argc, char **argv)
     {
         client_legth = sizeof(client_addr);
 
-        aux = recvfrom(sockfd, msg_in, 80, 0, (struct sockaddr *)&client_addr, &client_legth);
+        aux = recvfrom(sockfd, msg_in2, MAX_LENGHT+1, 0, (struct sockaddr *)&client_addr, &client_legth);
         ASSERT(aux != -1, "Error recibiendo datagrama: %s\n", strerror(errno));
 
-        printf("Recibida string: %s desde %s\n", msg_in, inet_ntoa(client_addr.sin_addr));
+        printf("Recibida string: %s desde %s\n", msg_in2, inet_ntoa(client_addr.sin_addr));
         
-        process_string(msg_in, msg_out);
+        process_string(msg_in2, msg_out2);
 
-        aux = sendto(sockfd, msg_out, 80, 0, (struct sockaddr *)&client_addr, client_legth);
+        aux = sendto(sockfd, msg_out2, strlen(msg_out2)+1, 0, (struct sockaddr *)&client_addr, client_legth);
         ASSERT(aux != -1, "Error enviando datagrama: %s\n", strerror(errno));
     }
+
     close(sockfd);
 }
 
 void process_string(char *msg_in, char *msg_out)
 {
-    strncpy(msg_out, msg_in, 80);
+    strncpy(msg_out,msg_in,MAX_LENGHT);
+    
     for(int i = 0; i < strlen(msg_in); i++){
         if(msg_in[i]>64 && msg_in[i]<91) msg_out[i]+=32;
         else if(msg_in[i]>96 && msg_in[i]<123) msg_out[i]-=32;
